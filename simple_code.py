b75c17b81c6d51e63f261eb3761ce20f20a2e579
Diksha161192 <dikshashetty1611@gmail.com>
2025-11-05 01:47:04 +0530

    Add files via upload
    Simple python script to print the details of the members of team aspartic

---
+++ simple_code.py
@@ -0,0 +1,25 @@
+team_aspartic = [
+{
+"Name": "Diksha Shetty", "Slack_username": "Diksha Shetty", "Country":"India", "Hobby": "Reading", "Affiliations": "Bioinformatics", "Favourite gene": "MYC", "Sequence": "GGAGTTTATTCATAACGCGCTCTCCAAGTATACGTGGCAATGCGTT"
+},
+{
+"Name":"Akinjide", "Slack_username": "Akinjide Akinowose", "Country":"Nigeria", "Hobby": "Coding", "Affiliations": "Biomedical informatics", "Favourite gene": "PTEN", "Sequence": "ATGACAGCCATCATCAAAGAG"
+},
+{
+"Name": "Fakhia Mubashir", "Slack_username": "Fakhia Mubashir", "Country":"Pakistan", "Hobby": "Reading", "Affiliations": "None", "Favourite gene": "TRPV6", "Sequence": "ATCGTCGCCCGATACGATCCAGTA"
+},
+{
+"Name": "Funmilayo", "Slack_username": "Funmilayo Christianah Ligali", "Country":"Nigeria", "Hobby": "Cooking", "Affiliations": "Biochemistry", "Favourite gene": "TP53", "Sequence": "ATGGAGGAGCCGCAGTCAGA"
+},
+{
+"Name": "Niraj Pun Magar", "Slack_username": "Niraj Pun Magar", "Country":"Nepal", "Hobby": "UX design", "Affiliations": "Bioinformatics", "Favourite gene": "APOE", "Sequence": "ATGCATGCGCGCATGC"
+},
+{
+"Name": "Himanshu Pundir", "Slack_username": "HIMANSHU PUNDIR", "Country":"India", "Hobby": "Editing", "Affiliations": "Bioinformatics", "Favourite gene": "SIR1", "Sequence": "ATGTCTATAAAAGGAAAT"
+},
+{
+"Name": "Sri Sathya Sandilya Garemilla", "Slack_username": "Garemilla Sandilya", "Country":"United States of America", "Hobby": "Coding", "Affiliations": "Bioinformatics and Molecular Biochemistry", "Favourite gene": "AP1", "Sequence": "AACCGCATCTGCAGCGAGCATCTGAGAAGCCAAGACTGAGCCGGCGGCCGCGGCGCAGCGAACGAGCAGT"
+},
+]
+
+print(team_aspartic)
